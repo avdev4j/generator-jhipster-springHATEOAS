fbf0561cd623b38cd4de21ab2dc4401860fe7e00
Anthony <anthony-externe.viard@edf.fr>
2018-07-10 01:35:39 +0200

    add templates for user HATEOAS support

--- README.md
+++ README.md
@@ -50,6 +50,31 @@ To use this blueprint, run the below command
 jhipster --blueprint springHATEOAS
 ```
 
+Please consider this responses from my blueprint test to generate a new project.
+At this moment only Monolithic/JWT/SQL/Maven config work with Spring HATEOAS.
+ 
+```
+? Which *type* of application would you like to create? Monolithic application (recommended for simple projects)
+? What is the base name of your application? test_blueprint_HATEOAS
+Trying to use blueprint generator-jhipster-springHATEOAS
+? What is your default Java package name? fr.avd.com
+? Do you want to use the JHipster Registry to configure, monitor and scale your application? No
+? Which *type* of authentication would you like to use? JWT authentication (stateless, with a token)
+? Which *type* of database would you like to use? SQL (H2, MySQL, MariaDB, PostgreSQL, Oracle, MSSQL)
+? Which *production* database would you like to use? MySQL
+? Which *development* database would you like to use? H2 with disk-based persistence
+? Do you want to use the Spring cache abstraction? Yes, with the Ehcache implementation (local cache, for a single node)
+? Do you want to use Hibernate 2nd level cache? Yes
+? Would you like to use Maven or Gradle for building the backend? Maven
+? Which other technologies would you like to use?
+? Would you like to enable internationalization support? Yes
+? Please choose the native language of the application English
+? Please choose additional languages to install
+? Besides JUnit and Jest, which testing frameworks would you like to use?
+? Would you like to install other generators from the JHipster Marketplace? No
+
+```
+ 
 ## During development of this blueprint
 
 1. On blueprint folder `yarn link`
--- generators/server/files.js
+++ generators/server/files.js
@@ -2,13 +2,37 @@ const constants = require('generator-jhipster/generators/generator-constants');
 
 const INTERPOLATE_REGEX = constants.INTERPOLATE_REGEX;
 const CURRENT_SERVER_TEMPLATE_PATH = '';
+const SERVER_MAIN_SRC_DIR = constants.SERVER_MAIN_SRC_DIR;
+const SERVER_TEST_SRC_DIR = constants.SERVER_TEST_SRC_DIR;
 
 const files = {
-    myModule: [
+    serverBuild: [
         {
             condition: generator => generator.buildTool === 'maven',
             templates: [
                 {file: 'pom.xml', options: {interpolate: INTERPOLATE_REGEX}}
+            ]
+        }
+    ],
+    serverJavaUserManagement: [
+        {
+            condition: generator => !generator.skipUserManagement,
+            path: SERVER_MAIN_SRC_DIR,
+            templates: [
+                /* User management java service files */
+                { file: 'package/service/UserService.java', renameTo: generator => `${generator.javaDir}service/UserService.java` },
+                /* User management java web files */
+                { file: 'package/service/dto/UserDTO.java', renameTo: generator => `${generator.javaDir}service/dto/UserDTO.java` },
+                { file: 'package/web/rest/UserResource.java', renameTo: generator => `${generator.javaDir}web/rest/UserResource.java` },
+                { file: 'package/service/mapper/UserMapper.java', renameTo: generator => `${generator.javaDir}service/mapper/UserMapper.java` }
+            ]
+        },
+        {
+            condition: generator => !generator.skipUserManagement,
+            path: SERVER_TEST_SRC_DIR,
+            templates: [
+                { file: 'package/web/rest/UserResourceIntTest.java', renameTo: generator => `${generator.testDir}web/rest/UserResourceIntTest.java` },
+                { file: 'package/web/rest/AccountResourceIntTest.java', renameTo: generator => `${generator.testDir}web/rest/AccountResourceIntTest.java` }
             ]
         }
     ]
